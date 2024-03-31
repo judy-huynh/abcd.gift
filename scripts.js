a23494edb84e9c59ffa06e3ac782e94e039dc62e
j00by <judy.h@nyu.edu>
2024-03-30 21:39:07 -0400

    buttons activate

--- scripts.js
+++ scripts.js
@@ -1,4 +1,31 @@
-function googleSearch() {
-    var query = document.getElementById('searchBox').value;
-    window.open('https://www.google.com/search?q=' + encodeURIComponent(query));
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var searchButton = document.getElementById('searchButton');
+    var luckyButton = document.getElementById('luckyButton');
+    var searchBox = document.getElementById('searchBox');
+
+    searchButton.addEventListener('click', function() {
+        googleSearch();
+    });
+
+    luckyButton.addEventListener('click', function() {
+        feelingLuckySearch();
+    });
+
+    searchBox.addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            googleSearch();
+        }
+    });
+
+    function googleSearch() {
+        var query = searchBox.value;
+        var searchURL = 'https://www.google.com/search?q=' + encodeURIComponent(query);
+        window.open(searchURL, '_blank');
+    }
+
+    function feelingLuckySearch() {
+        var query = searchBox.value;
+        var luckyURL = 'https://www.google.com/search?btnI=I&q=' + encodeURIComponent(query);
+        window.open(luckyURL, '_blank');
+    }
+});
